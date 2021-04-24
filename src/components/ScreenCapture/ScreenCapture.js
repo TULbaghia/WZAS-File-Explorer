052640aca638bea1320124e2eede3f9aef2907c7
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-04-24 16:18:21 +0200

    Fix starting

--- src/components/ScreenCapture/ScreenCapture.js
+++ src/components/ScreenCapture/ScreenCapture.js
@@ -1,25 +1,4 @@
 import React from 'react';
-import styles from './ScreenCapture.module.css';
-
-const videoElem = document.getElementById("video");
-const startElem = document.getElementById("start");
-const stopElem = document.getElementById("stop");
-
-function ScreenCapture(props) {
-    return (
-        <div>
-            <p>This example shows you the contents of the selected part of your display.
-                Click the Start Capture button to begin.</p>
-
-            <p>
-                <button id="start" onClick={() => startCapture()}>Start Capture</button>
-                &nbsp;
-                <button id="stop">Stop Capture</button>
-            </p>
-            <video id="video" autoPlay></video>
-        </div>
-);
-}
 
 const displayMediaOptions =
     {
@@ -37,9 +16,9 @@ const displayMediaOptions =
 //     }
 // , false);
 
-function dumpOptionsInfo()
+function dumpOptionsInfo(param)
     {
-        const videoTrack = videoElem.srcObject.getVideoTracks()[0];
+        const videoTrack = param.srcObject.getVideoTracks()[0];
 
         console.info("Track settings:");
         console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
@@ -49,8 +28,25 @@ function dumpOptionsInfo()
 
 async function startCapture()
     {
+        const videoElem = document.getElementById("video");
         videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-        dumpOptionsInfo();
+        dumpOptionsInfo(videoElem);
     }
+
+function ScreenCapture(props) {
+    return (
+        <div>
+            <p>This example shows you the contents of the selected part of your display.
+                Click the Start Capture button to begin.</p>
+
+            <video id="video" autoPlay/>
+            <p>
+                <button id="start" onClick={() => startCapture()}>Start Capture</button>
+                &nbsp;
+                <button id="stop">Stop Capture</button>
+            </p>
+        </div>
+    );
+}
 
 export default ScreenCapture;
