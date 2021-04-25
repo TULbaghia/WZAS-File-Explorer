66aed3fa4eb27dfe60fc386c10d6fb66fee5d0a2
Wojciech Sowa <224429@edu.p.lodz.pl>
2021-04-25 20:45:27 +0200

    Stop using microphone after recording

--- src/components/AudioCapture/AudioCapture.js
+++ src/components/AudioCapture/AudioCapture.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 var mediaRecorder;
 var chunks = [];
+var audioStream;
 
 async function startCapture() {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia (
+
+        navigator.mediaDevices.getUserMedia(
             {
                 audio: true
             })
-            .then(function(stream) {
+            .then(function (stream) {
+                audioStream = stream;
                 mediaRecorder = new MediaRecorder(stream);
                 mediaRecorder.start();
-                mediaRecorder.ondataavailable = function(e) {
+                mediaRecorder.ondataavailable = function (e) {
                     chunks.push(e.data);
                 }
             })
@@ -22,18 +25,20 @@ async function startCapture() {
 function stopCapture() {
 
     mediaRecorder.stop();
-    mediaRecorder.onstop = function(e) {
+    mediaRecorder.onstop = function () {
         const clipName = prompt('Enter a name for your sound clip');
 
-        const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+        const blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
         chunks = [];
         const audioURL = window.URL.createObjectURL(blob);
 
-        var link = document.createElement("a");
+        let link = document.createElement("a");
         link.href = audioURL;
         link.download = clipName + ".opus";
-        link.innerHTML = "Click here to download the file";
-        document.body.appendChild(link);
+        link.click();
+        audioStream.getTracks().forEach(function (track) {
+            track.stop()
+        })
     }
 }
 
