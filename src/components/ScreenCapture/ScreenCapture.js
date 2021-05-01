f4bf187d85d2e412b3aa27744f82d12003724f42
Wojciech Sowa <224429@edu.p.lodz.pl>
2021-05-01 20:58:22 +0200

    Add video recording

--- src/components/ScreenCapture/ScreenCapture.js
+++ src/components/ScreenCapture/ScreenCapture.js
@@ -1,27 +1,65 @@
 import React from 'react';
 
+var mediaRecorder;
+var chunksVideo = [];
+var chunksAudio = [];
+var videoStream;
+var audioStream;
+
 const displayMediaOptions = {
     video: {
         cursor: "always"
     },
-    audio: false
+    audio: true,
+    audioBitsPerSecond : 128000,
+    videoBitsPerSecond : 2500000,
+    mimeType : 'video/mp4'
 };
 
 async function startCapture() {
-    const videoElem = getVideoElement();
-    videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    await navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
+        .then(function (stream) {
+            videoStream = stream;
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start();
+            mediaRecorder.ondataavailable = function (e) {
+                chunksVideo.push(e.data);
+            }
+        });
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        await navigator.mediaDevices.getUserMedia(
+            {
+                audio: true
+            })
+            .then(function (stream) {
+                audioStream = stream;
+                mediaRecorder = new MediaRecorder(stream);
+                mediaRecorder.start();
+                mediaRecorder.ondataavailable = function (e) {
+                    chunksAudio.push(e.data);
+                }
+            })
+    }
 }
 
 function stopCapture() {
-    let video = getVideoElement();
-    let tracks = video.srcObject.getTracks();
+    mediaRecorder.stop();
+    mediaRecorder.onstop = function () {
+        const clipName = prompt('Wybierz nazwę pliku');
 
-    tracks.forEach(track => track.stop());
-    video.srcObject = null;
-}
+        const blob = new Blob([chunksVideo, chunksAudio], {type: chunksVideo[0].type});
+        chunksVideo = [];
+        chunksAudio = [];
+        const videoURL = window.URL.createObjectURL(blob);
 
-function getVideoElement() {
-    return document.getElementById("video");
+        const link = document.createElement("a");
+        link.href = videoURL;
+        link.download = clipName;
+        link.click();
+        videoStream.getTracks().forEach(function (track) {
+            track.stop()
+        })
+    }
 }
 
 function ScreenCapture() {
@@ -31,7 +69,6 @@ function ScreenCapture() {
                 <button onClick={() => startCapture()}>Rozpocznij nagrywanie ekranu</button>
                 <button onClick={() => stopCapture()}>Zakończ nagrywanie ekranu</button>
             </p>
-            <video id="video" autoPlay/>
         </div>
     );
 }
